56868655da4312bc8598fc24f85b90d1bc7cb060
gohil rohit <gohilrohit309@gmail.com>
2024-06-28 18:26:42 +0530

    add flies

--- firebase-app/src/Components/Home.jsx
+++ firebase-app/src/Components/Home.jsx
@@ -1,10 +1,35 @@
+import { useState } from "react";
 
 
 
-const Home = ()=>{
-    return(
+const Home = () => {
+
+    let [user, setUser] = useState({});
+
+    const getValue = (e) => {
+        let name = e.target.name;
+        let value = e.target.value;
+        setUser({ ...user, [name]: value });
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(user)
+    }
+
+    return (
         <>
-            <h1>Hello Wolrd</h1>
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <div>
+                    <label htmlFor="">Email :- </label>
+                    <input type="text" name="email" value={user.email ? user.email : ""} placeholder="Enter Your Email" onChange={(e) => getValue(e)} />
+                </div>
+                <div>
+                    <label htmlFor="">Password :- </label>
+                    <input type="text" name="pass" value={user.pass ? user.pass : ""} placeholder="Enter Your Password" onChange={(e) => getValue(e)} />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
         </>
     )
 }
---
+++ firebase-app/src/firebase.jsx
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBIx540I42nrz1LFgmU5bkmZXD-hB5ZR2M",
+  authDomain: "fir-project-faa78.firebaseapp.com",
+  projectId: "fir-project-faa78",
+  storageBucket: "fir-project-faa78.appspot.com",
+  messagingSenderId: "193036232714",
+  appId: "1:193036232714:web:1fc5acf50be5ab4cf717f3",
+  measurementId: "G-D40KL74T1N"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
